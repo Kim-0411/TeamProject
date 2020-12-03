58fbaaf6212b6ca3fd7fde9c800bdb8dc8ab1da3
Jeongho Y <31297335+wjdgh7587@users.noreply.github.com>
2020-12-03 16:38:41 +0900

    ADD Second git project
    2020.12.03
    
    From Jeongho.author

---
+++ egit_spring_test/src/main/java/com/git/test/egit_spring_test/EgitSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.git.test.egit_spring_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgitSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgitSpringTestApplication.class, args);
+	}
+
+}
---
+++ egit_spring_test/src/test/java/com/git/test/egit_spring_test/EgitSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.test.egit_spring_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgitSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
